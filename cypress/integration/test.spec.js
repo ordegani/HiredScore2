33f76cae81e8726975f83f8ec0d10acf149a52e2
ordegani <oradeganii@gmail.com>
2022-02-21 16:16:58 +0200

    first commit

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -6,13 +6,17 @@ context('Navigation', () => {
         cy.get('.w-dropdown').last().trigger('mouseover')
         cy.get('a[href*="/careers"]').first().click({ multiple: true })
         cy.url('https://www.hiredscore.com/careers#career-listing')
-        cy.get('.job-grid-title').first().click({force: true})
+        cy.get('.job-grid-title').first().click({ force: true })
         cy.get('a[href*="/careers"]').last().click({ multiple: true })
         cy.get('.w-dropdown').eq(2).trigger('mouseover')
         cy.get('a[href*="/press"]').first().click({ multiple: true })
         cy.url('https://www.hiredscore.com/press')
-
+        //ALTERNATIVE:
+        console.log(cy.location()
+        );
+        // TODO BONUS
+        //console.log(('featured-posts__grid w-dyn-items >div'));
     });
 });
-       
+
 //   NOTES wrong pattern: cy.get('.dropdown-link no-icon w-dropdown-link').should('have.text', 'Careers').click()  
