6639fae62fc9682a5773f7abf04ee38db97a0cef
ordegani <oradeganii@gmail.com>
2022-02-21 15:27:23 +0200

    first commit

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -6,5 +6,7 @@ context('Navigation', () => {
         cy.get('.w-dropdown').last().trigger('mouseover')
         //   cy.get('.dropdown-link no-icon w-dropdown-link').should('have.text', 'Careers').click()  
         cy.get('a[href*="/careers"]').first().click({ multiple: true })
+        cy.url('https://www.hiredscore.com/careers#career-listing')
+        cy.get('.job-grid-title').first().click({force: true})
     });
 });
